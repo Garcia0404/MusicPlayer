2986e29f537be85074cc4934ee4d6de777cd1b00
Garcia0404 <julioxenrique@gmail.com>
2024-06-04 15:46:03 -0500

    allLoaded homepage Main

--- src/pages/homepage/components/Card.jsx
+++ src/pages/homepage/components/Card.jsx
@@ -3,15 +3,11 @@ import { PlaySong,CardLoading } from '.'
 
 export const Card = ({ song, listen }) => {
   const [state, setState] = useState(false)
-  const [error,setError] = useState(false)
   const style = state ? 'opacity-1 flex' : 'opacity-0 hidden'
   return (
     <>
       <article className={`relative bg-[#232526] hover:bg-grayHoverMain transition-all ${style} rounded-[4px] gap-2 pe-2 cursor-pointer overflow-hidden group/item`}>
-        <img onLoad={() => setState(true)} onError={() => {setState(true);setError(true)}} className={`${error?'hidden':'flex'} w-14 h-full`} src={song.album.albumImg} alt={song.name} />
-        <div className={`${error ? 'flex' : 'hidden'} w-14 h-full p-3 bg-gray-300`} >
-          <img className='w-14 h-full'  src="/svg/loadingmusic.svg" alt={song.name} />
-        </div>
+        <img onLoad={() => setState(true)} className='flex w-14 h-full' src={song.album.albumImg} alt={song.name} />
         <div className='flex items-center flex-1 p-1 overflow-hidden'>
           <span className='text-xs font-semibold text-nowrap'>{song.name}</span>
         </div>
@@ -19,7 +15,6 @@ export const Card = ({ song, listen }) => {
           <PlaySong song={song} listen={listen} />
         </div>
       </article>
-      <CardLoading loading={!state}/>
     </>
 
   )
--- src/pages/homepage/components/Main.jsx
+++ src/pages/homepage/components/Main.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Card } from "."
 import { albums } from "../../../helper/music/albums"
 import { contexto } from "../../../context/AppContext"
-import { GeneralCard } from "./GeneralCard"
+import { GeneralCard, CardLoading } from "."
 export const Main = () => {
   const { recent } = useContext(contexto)
+  const [allLoaded, setAllLoaded] = useState(false)
   const songs = [
     [5, 5],
     [0, 4],
@@ -13,23 +14,47 @@ export const Main = () => {
     [4, 3],
     [3, 9]
   ]
+  const [loaded, setLoaded] = useState(Array(songs.length).fill(false))
+  const handleLoad = (index) => {
+    setLoaded(prevLoad => {
+      const newLoad = [...prevLoad]
+      newLoad[index] = true
+      return newLoad
+    })
+  }
+  useEffect(() => {
+    if (loaded.every((load) => load === true)) {
+      setAllLoaded(true)
+    }
+  }, [loaded])
   return (
     <main className='max-w-[700px] lg:max-w-full w-full mobile:mx-auto lg:mx-0 col-start-3 col-end-8 bg-secondary overflow-auto tablet:rounded-md'>
       <section className='bg-gradient-to-b from-[#545455] to-secondary to-80% text-white pt-10 px-6 pb-4 flex flex-col gap-3'>
         <h1 className='font-bold text-3xl mb-3'>Welcome !</h1>
         <main className="grid grid-cols-2 mobileLg:grid-cols-mainH gap-3">
           {
-            songs.map((song, index) => (
-              <Card song={albums[song[0]].canciones[song[1]]} key={index} listen={false} />
-            ))
+            allLoaded ?
+              songs.map((song, index) => (
+                <Card song={albums[song[0]].canciones[song[1]]} key={index} listen={false} />
+              )) :
+              songs.map((song, index) => (
+                <CardLoading loading={true} key={index}/>
+              ))
           }
+          <div className="hidden">
+            {
+              songs.map((song, index) => (
+                <img onLoad={() => handleLoad(index)} key={index} src={albums[song[0]].image} alt={albums[song[0].nombre]} />
+              ))
+            }
+          </div>
         </main>
       </section>
       <section className='p-6 text-white grid gap-2 mobileLg:grid-cols-mainH'>
         <h2 className="text-xl font-semibold col-start-1 col-end-[-1]">Heard Recently</h2>
-        {recent.length!==0 && recent.map((song, index) => (
-              <GeneralCard key={index} music={song} />
-            ))
+        {recent.length !== 0 && recent.map((song, index) => (
+          <GeneralCard key={index} music={song} />
+        ))
         }
       </section>
     </main>
