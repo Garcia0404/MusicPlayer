bffc79f0654e402d7a043a6e234a21c08130e09d
Garcia0404 <julioxenrique@gmail.com>
2024-12-29 20:33:33 -0500

    link preload y preconnect

--- src/pages/homepage/components/GeneralCard.jsx
+++ src/pages/homepage/components/GeneralCard.jsx
@@ -6,7 +6,7 @@ export const GeneralCard = ({ music }) => {
     <>
       <article className='flex-col gap-2 p-4 pb-8 bg-[#181818] hover:bg-[#262626] transition-all rounded-lg inline-flex group/item min-h-[280px] h-full text-white'>
         <div className='relative'>
-          <img className='h-auto w-full rounded-lg' loading='lazy' src={music.album.albumImg} alt={music.name} />
+          <img className='h-auto w-full rounded-lg' src={music.album.albumImg} alt={music.name} width='300px' height='300px'/>
           <div className='m-3 absolute bottom-0 right-0 scale-150 opacity-0 translate-y-3 duration-300 group/edit group-hover/item:opacity-100 group-hover/item:translate-y-0'>
             <PlaySong song={music} listen={false} />
           </div>
