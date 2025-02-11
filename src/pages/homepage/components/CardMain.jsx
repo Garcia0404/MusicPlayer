78cf23db0f7306b66f65b14a56325a52c39c7f20
Garcia0404 <julioxenrique@gmail.com>
2025-02-10 19:13:28 -0500

    songpage ui

--- src/pages/homepage/components/CardMain.jsx
+++ src/pages/homepage/components/CardMain.jsx
@@ -7,12 +7,12 @@ export const CardMain = ({ layout = false, ...albumid }) => {
       {
         layout ? (
           <>
-            <motion.div layoutId={`album-${albumid.nombre}`} className='z-20 mx-auto tablet:mx-0 w-40 h-40 mobileLg:w-auto bg-grayMain rounded overflow-hidden shadow-xl'>
+            <motion.div layoutId={`album-${albumid.nombre}`} transition={{duration:0.3}} className='z-20 mx-auto tablet:mx-0 w-40 h-40 mobileLg:w-auto bg-grayMain rounded overflow-hidden shadow-xl'>
               <motion.img className='w-40 h-40 mx-auto' src={albumid.image} alt={albumid.nombre} />
             </motion.div>
             <div className='overflow-hidden'>
               <span className='text-sm'>Album</span>
-              <h1 className='font-bold text-balance'>{albumid.nombre}</h1>
+              <motion.h1 animate={{y:[30,0],opacity:[0,1]}} transition={{duration:0.3,ease:"easeIn"}} className='font-bold text-balance'>{albumid.nombre}</motion.h1>
               <div className='flex text-base font-light gap-2 items-center'>
                 <span>The Strokes</span>
                 <Circle />
--- src/pages/songpage/components/CardSong.jsx
+++ src/pages/songpage/components/CardSong.jsx
@@ -38,7 +38,7 @@ export const CardSong = () => {
             animate="enter"
             layout
             style={{ height: "calc(100dvh - 64px)" }}
-            className='max-w-[700px] min-h-[350px] lg:max-w-full w-full mobile:mx-auto lg:mx-0 col-start-3 col-end-8 bg-secondary overflow-hidden mobileLg:rounded-md z-40 relative'>
+            className='max-w-[700px] min-h-[600px] lg:max-w-full w-full mobile:mx-auto lg:mx-0 col-start-3 col-end-8 bg-secondary z-40 relative'>
             <motion.div className='-z-10 absolute top-0 left-0 w-full h-full' animate={{ background: "linear-gradient(#32aac8,transparent)" }}></motion.div>
             <article className='flex justify-center flex-col px-5 text-white max-w-[700px] mx-auto h-full'>
               <BackButton callback={() => { setShowFooter(true) }} />
