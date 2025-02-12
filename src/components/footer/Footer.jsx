f5ca19bd8f49b4cdfb5991a96b01e6b5b0e5d43e
Garcia0404 <julioxenrique@gmail.com>
2025-02-12 07:02:21 -0500

    responsive nav

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,11 +20,11 @@ export const Footer = () => {
             variants={variants}
             initial="initial"
             animate="enter"
-            className={`${style} w-full bg-transparent sticky right-0 bottom-20 tablet:bottom-0 tablet:m-0 z-10`}
+            className={`${style} w-full bg-transparent sticky right-0 bottom-20 lg:bottom-0 lg:m-0 z-10 max-lg:max-w-xl mx-auto`}
           >
-            <div className='flex tablet:justify-between items-center tablet:mx-0 bg-red9 tablet:bg-black rounded-lg p-3 gap-4 mx-2'>
+            <div className='flex justify-between items-center lg:mx-0 bg-red9 lg:bg-black rounded-lg p-3 gap-4 mx-2'>
               <CardSong />
-              <main className='flex tablet:items-center flex-grow tablet:mx-0 flex-col gap-1 col-start-4 col-end-8'>
+              <main className='flex lg:items-center flex-grow lg:mx-0 flex-col gap-1 col-start-4 col-end-8'>
                 <section className='max-mobileLg:ms-auto flex items-center gap-3 mobileLg:gap-4 justify-center'>
                   <span className='hidden mobileLg:block'>
                     <RandomBtn />
@@ -38,13 +38,13 @@ export const Footer = () => {
                     <RepeatBtn />
                   </span>
                 </section>
-                <section className='hidden text-white font-extralight text-sm tablet:flex items-center gap-2 max-w-3xl tablet:w-full'>
+                <section className='hidden text-white font-extralight text-sm lg:flex items-center gap-2 max-w-3xl lg:w-full'>
                   <span>{time}</span>
                   <SliderMusic />
                   <span>{duration}</span>
                 </section>
               </main>
-              <div className='hidden tablet:flex items-center gap-3 me-4 justify-end col-start-8 col-end-11'>
+              <div className='hidden lg:flex items-center gap-3 me-4 justify-end col-start-8 col-end-11'>
                 <MuteBtn />
                 <Slider />
                 <audio onEnded={handleSongEnded} ref={ref} src={music.music}></audio>
--- src/components/nav/NavbarMobile.jsx
+++ src/components/nav/NavbarMobile.jsx
@@ -8,7 +8,7 @@ export const NavbarMobile = () => {
     ['Favorites','/favorites']
   ]
   return (
-    <nav className='tablet:hidden w-full h-16 bg-[rgba(0,0,0,0.8)] backdrop-blur-md flex justify-center sticky bottom-0 right-0 z-50'>
+    <nav className='lg:hidden w-full h-16 bg-[rgba(0,0,0,0.8)] backdrop-blur-md flex justify-center sticky bottom-0 right-0 z-50'>
       <div className='flex gap-5 mobileLg:gap-8 py-2 mobileLg:p-4 items-center'>
         {
           links.map((link,index)=> (
