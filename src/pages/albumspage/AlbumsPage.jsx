fa6d2a8a92265cbb9cedb7da06bca7d2e995ee3f
Garcia0404 <julioxenrique@gmail.com>
2025-02-13 06:47:34 -0500

    useScroll albumsPage

--- src/pages/albumspage/AlbumsPage.jsx
+++ src/pages/albumspage/AlbumsPage.jsx
@@ -1,24 +1,29 @@
 import { CardAlbum } from '../../components'
-import { useContext, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { contexto } from '../../context/AppContext'
-import { motion } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 export const AlbumsPage = () => {
+  const ref = useRef()
+  const { scrollY } = useScroll({ container: ref })
+  const opacity = useTransform(scrollY, [0, 100], [0, 1])
+
   const { data } = useContext(contexto)
   const [isLoaded, setIsLoaded] = useState(false)
   return (
-    <div className='max-w-[700px] lg:max-w-full w-full mobile:mx-auto lg:mx-0 col-start-3 col-end-8 bg-secondary overflow-auto tablet:rounded-md border-white'>
+    <div ref={ref} className='max-w-[700px] lg:max-w-full w-full mobile:mx-auto lg:mx-0 col-start-3 col-end-8 bg-secondary overflow-auto tablet:rounded-md border-white'>
       <header className='relative w-full flex items-center justify-between flex-col bg-black'>
         <img onLoad={() => setIsLoaded(true)} className='hidden' src="/img/the-strokes.webp" alt="background-theStrokes" />
-        <motion.div 
-          initial={{opacity:0}}
+        <motion.div
+          initial={{ opacity: 0 }}
           animate={{ opacity: isLoaded ? 1 : 0 }}
           transition={{ duration: 0.5 }}
           className='h-[270px] max-w-[900px] w-full overflow-hidden bg-center bg-cover' style={{ backgroundImage: "url(/img/the-strokes.webp)" }}>
         </motion.div>
+        <motion.div style={{ opacity }} className='absolute top-0 left-0 w-full h-full bg-[#603000]'></motion.div>
         <h1 className='absolute bottom-10 left-0 text-white text-5xl mobileLg:text-7xl tablet:text-8xl font-bold p-5'>The Strokes</h1>
       </header>
       <main className='relative flex flex-col gap-3 pt-8 px-3 sm:px-4 pb-4 text-white overflow-auto'>
-        <div className='absolute top-0 left-0 w-full h-48 bg-gradient-to-b from-[#562D03]'></div>
+        <div className='absolute top-0 left-0 w-full h-48 bg-gradient-to-b from-[#603000]'></div>
         <h2 className='text-white text-3xl font-medium z-10'>Albums</h2>
         <ul className='z-10'>
           {
